10f2a9f10e33391aa4f27286badf3aff8b0495ab
David Murillo <dmz866@hotmail.com>
2025-03-14 10:28:47 -0500

    auth added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { envs } from './config/envs';
 import { GitHubController } from './controllers/git-hub/git-hub.controller';
+import { GithubMiddleware } from './middlewares/git-hub.middleware';
 
 function main() {
     const app = express();
     const controller = new GitHubController();
 
     app.use(express.json());
-    app.post('/api/git-hub', controller.webHookHandler);
+    app.post('/api/git-hub', [GithubMiddleware.verifyGitHubSignature], controller.webHookHandler);
 
     app.listen(envs.PORT, () => console.log('running app'));
 }
